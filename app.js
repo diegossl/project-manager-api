41d2e390f3e29ef42ce09b148933fdfdc8ba231d
diegossl <diegossl94@gmail.com>
2020-07-29 23:20:48 -0300

    View removed

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
@@ -14,14 +13,10 @@ const app = express()
 
 app.use(cors())
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(usersRouter)
 app.use(projectsRouter)
