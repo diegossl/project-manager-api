95cb384a254242fc5a7f1862334f3c4c28fd6018
mercyaandreza <deza_mercya@hotmail.com>
2020-07-31 11:58:11 -0300

    auth route changed

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const { verifyToken } = require('../app/middlewares/authJwt')
 const AuthController = require('../app/controllers/AuthController')
 
 const router = express.Router()
 
 router.post('/login', AuthController.login)
-router.get('/logout', AuthController.logout)
+router.get('/logout', verifyToken, AuthController.logout)
 
 module.exports = router
